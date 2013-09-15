3362c3fea2c228f27db687ef21521d8cc252b484
Jon <jondell518@gmail.com>
2013-09-15 17:24:18 -0400

    game of life is functioning?

--- main.js
+++ main.js
@@ -4,19 +4,31 @@ c.addEventListener('click',function(evt){
 	var indexX = Math.floor(evt.clientX/(cellDimension+offset));
 	var indexY = Math.floor(evt.clientY/(cellDimension+offset));
 	console.log("in event listener" + 	cells[indexX][indexY].b);
-	cells[indexX][indexY].b = false;
+	if(loopStarted == false)
+	{
+		cells[indexX][indexY].b = false;
 	//cells[indexX][indexY].clear();
-	//cells[indexX][indexY].draw();
+	cells[indexX][indexY].draw();
 	console.log(indexX + " " + indexY);
+}
+else
+	console.log("Loop already started fool!");
 });
+var loopStarted = false;
+var startButton = document.getElementById("startButton");
+startButton.addEventListener('click', function(){
+	console.log("Process started!");
+	loopStarted = true;
+	setInterval(loop, 1000/fps)
+})
 var ctx = c.getContext("2d");
-var fps = 30;
+var fps = 10;
 var x = 0;
 var y = 0;
-var cellDimension = 25;
-var numCellsW = 10;
-var numCellsH = 10;
-var offset = 5;
+var cellDimension = 10;
+var numCellsW = 250;
+var numCellsH = 250;
+var offset = 2;
 var bool = true;
 
 var cell = function(x,y,bool) {
@@ -24,11 +36,10 @@ var cell = function(x,y,bool) {
 	returnObj.x = x;
 	returnObj.y = y;
 	returnObj.b = bool;
-	returnObj.draw = function(){
-		
+	returnObj.reDraw = function(){
 		if(returnObj.b == true) {
-			//console.log(returnObj.b)
-			ctx.fillStyle = "#cc00cc";
+			//console.log("they are the same!");
+			ctx.fillStyle = "#FFFFFF";
 			var posX = returnObj.x*(cellDimension+offset);
 			var posY = returnObj.y*(cellDimension+offset);
 			ctx.fillRect(posX,posY,cellDimension, cellDimension);
@@ -40,7 +51,24 @@ var cell = function(x,y,bool) {
 			var posY = returnObj.y*(cellDimension+offset);
 			ctx.fillRect(posX,posY,cellDimension, cellDimension);
 		}
-			
+	};
+	returnObj.draw = function(){
+		
+		if(returnObj.b == true) {
+			//console.log(returnObj.b)
+			ctx.fillStyle = "#FFFFFF";
+			var posX = returnObj.x*(cellDimension+offset);
+			var posY = returnObj.y*(cellDimension+offset);
+			ctx.fillRect(posX,posY,cellDimension, cellDimension);
+		}
+		if(returnObj.b == false)
+		{
+			ctx.fillStyle = '#000000';
+			var posX = returnObj.x*(cellDimension+offset);
+			var posY = returnObj.y*(cellDimension+offset);
+			ctx.fillRect(posX,posY,cellDimension, cellDimension);
+		}
+
 	};
 
 	return returnObj;
@@ -55,20 +83,87 @@ var init = function(){
 		cells[i] = [];
 		for (var j = 0; j < numCellsH; j++) {
 			cells[i][j] = cell(i,j, bool);
+			cells[i][j].draw();
 		};
 	}
 
 };
+var update = function(tempCells){
 
+
+
+	for(var i =1; i<numCellsW-1;i++)
+	{
+		for(var j = 1; j <numCellsH-1; j++)
+		{
+			var liveCounter = 0;
+			if(cells[i+1][j].b === false)
+			{
+				liveCounter++;
+			}
+			if(cells[i+1][j+1].b === false)
+			{
+				liveCounter++;
+			}
+			if(cells[i+1][j-1].b === false)
+			{
+				liveCounter++;
+			}
+			if(cells[i][j+1].b === false)
+			{
+				liveCounter++;
+			}
+			if(cells[i][j-1].b === false)
+			{
+				liveCounter++;
+			}
+			if(cells[i-1][j-1].b === false)
+			{
+				liveCounter++;
+			}
+			if(cells[i-1][j].b === false)
+			{
+				liveCounter++;
+			}
+			if(cells[i-1][j+1].b === false)
+			{
+				liveCounter++;
+			}
+
+			if(cells[i][j].b === true && liveCounter === 3)
+			{
+				tempCells[i][j].b = false;
+			}
+			if(cells[i][j].b === false && liveCounter === 2)
+			{
+				tempCells[i][j].b = false;
+			}
+			if(cells[i][j].b === false && liveCounter === 3)
+			{
+				tempCells[i][j].b = false;
+			}
+			else
+				tempCells[i][j].b = true;
+		}
+	}
+
+
+
+
+
+
+}
 var loop = function(){
-	
+	var tempCells = cells.slice(0,cells.length)
+	console.log("tempcells initialized: " + tempCells);
+	update(tempCells);
+	//cells = tempCells.slice(0,tempCells.length);
 	for(var i = 0; i < numCellsW; i++)
 	{
 		for (var j = 0; j < numCellsH; j++) {
-			cells[i][j].draw();
+			cells[i][j].reDraw();
 		};
 	}
 	// console.log("loop finished being called!");
 }
 init();
-setInterval(loop, 1000/fps)
